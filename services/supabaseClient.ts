17062ee92ac7f9c632d5ce73fb9f719cc57faaf0
Diego Valdés <156203455+DIEGO-VALDES-R@users.noreply.github.com>
2025-11-25 19:09:40 -0500

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zvtkkdjawdqrgpawghni.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2dGtrZGphd2RxcmdwYXdnaG5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNDY2NDAsImV4cCI6MjA3OTYyMjY0MH0.p4OqCcl_LrIey0EppJnEYNEzVCLf2kFZNwuqDqvoJlo';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://zvtkkdjawdqrgpawghni.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'tu-key-aqui';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
